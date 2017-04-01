c2469e2439c80cae0c703eedbd813e8e75b0f8c4
Saranya Ganapathy <SaranyaGanapathy@Saranyas-MBP.home>
2017-04-01 17:23:13 -0400

    checkpoint 3 completed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,21 +2,25 @@
     function config($stateProvider, $locationProvider) {
         $locationProvider
             .html5Mode({
-                enabled: true,
-                requireBase: false
+            enabled: true,
+            requireBase: false
         });
-        
+
         $stateProvider
             .state('landing', {
-                url: '/',
-                templateUrl: '/templates/landing.html'
+            url: '/',
+            templateUrl: '/templates/landing.html'
         })
-        .state('album', {
+            .state('album', {
             url: '/album',
             templateUrl: '/templates/album.html'
+        })
+            .state('collection', {
+            url: '/collection',
+            templateUrl: '/templates/collection.html'
         });
     }
-    
+
     angular
         .module('blocJams', ['ui.router'])
         .config(config);
