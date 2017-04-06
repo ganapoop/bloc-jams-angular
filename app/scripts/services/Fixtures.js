2c349c56f154f7f71cc2f685f61c2c4a29ca6ee0
Saranya Ganapathy <SaranyaGanapathy@Saranyas-MBP.home>
2017-04-06 18:23:32 -0400

    checkpoint 6 assignment completed

--- app/scripts/services/Fixtures.js
+++ app/scripts/services/Fixtures.js
@@ -31,9 +31,20 @@
                 { title: 'Wrong phone number', duration: '2:15' }
             ]
         };
+
         Fixtures.getAlbum = function() {
             return albumPicasso;
         };
+
+        Fixtures.getCollection = function(numberOfAlbums){
+            this.albums = [];
+            for (var i=0; i < 12; i++) {
+                this.albums.push(angular.copy(albumPicasso));
+
+            }
+            
+            return albums;
+        }
         retrun Fixtures;
     }
 
