384fd2690bfe6a092f0711b359337672d4ad74fb
Saranya Ganapathy <SaranyaGanapathy@Saranyas-MBP.home>
2017-04-06 19:50:16 -0400

    fixes to routing, controllers, and album

--- app/scripts/services/Fixtures.js
+++ app/scripts/services/Fixtures.js
@@ -38,14 +38,15 @@
 
         Fixtures.getCollection = function(numberOfAlbums){
             this.albums = [];
+            
             for (var i=0; i < 12; i++) {
                 this.albums.push(angular.copy(albumPicasso));
 
             }
             
-            return albums;
+            return this.albums;
         }
-        retrun Fixtures;
+        return Fixtures;
     }
 
     angular
